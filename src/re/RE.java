3b173f27229d01abcfe3a06ea7d876822bbac19b
Sagechen1 <Chrisschenzixiao@gmail.com>
2021-04-27 09:27:40 -0600

    update

--- src/re/RE.java
+++ src/re/RE.java
@@ -1,11 +1,18 @@
 package re;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
 import fa.nfa.NFA;
+import fa.State;
+import fa.nfa.NFAState;
 
 public class RE implements REInterface{
-    String RegEx;
+    String regEx;
+
+
+    //constructor of RE
     public RE(String RegEx){
-        this.RegEx = RegEx;
+        this.regEx = RegEx;
 
     }
 
@@ -13,4 +20,27 @@ public class RE implements REInterface{
     public NFA getNFA() {
         return null;
     }
+
+    /*
+    * Generate an NFA from a regular expression
+    *
+    * */
+    private NFA regEx(){
+
+
+
+
+        return null; //place holder, delete later
+    }
+
+
+    /**
+     *
+     *
+     * ***/
+    private char peek(){
+            return regEx.charAt(0);
+
+    }
+
 }
