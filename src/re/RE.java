982d44a02b35222d67800be849c9b4bb261909a3
Sagechen1 <Chrisschenzixiao@gmail.com>
2021-04-27 10:49:31 -0600

    update

--- src/re/RE.java
+++ src/re/RE.java
@@ -2,32 +2,35 @@ package re;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
+
 import fa.nfa.NFA;
 import fa.State;
 import fa.nfa.NFAState;
 
-public class RE implements REInterface{
+public class RE implements REInterface {
     String regEx;
 
 
     //constructor of RE
-    public RE(String RegEx){
+    public RE(String RegEx) {
         this.regEx = RegEx;
 
     }
 
+
+    /**
+     * @return The NFA that is built by a given regular expression
+     * */
     @Override
     public NFA getNFA() {
-        return null;
+        return regEx();
     }
 
     /*
-    * Generate an NFA from a regular expression
-    *
-    * */
-    private NFA regEx(){
-
-
+     * Generate an NFA from a regular expression
+     *
+     * */
+    private NFA regEx() {
 
 
         return null; //place holder, delete later
@@ -35,12 +38,98 @@ public class RE implements REInterface{
 
 
     /**
+     * returns the next item of input without consuming it
      *
-     *
-     * ***/
-    private char peek(){
-            return regEx.charAt(0);
+     * @return
+     ***/
+    private char peek() {
+        return regEx.charAt(0);
 
     }
+
+    /**
+     * returns the next item of input and consumes it
+     */
+    private char next() {
+        char c = peek();
+        eat(c);
+
+        return 0;//placer holder
+    }
+
+    private void eat(char c) {
+        if (peek() == c) {
+            this.regEx = this.regEx.substring(1);
+        } else {
+            throw new
+                    RuntimeException("Expected: " + c + "; got: " + peek());
+        }
+    }
+
+    private boolean more() {
+        return regEx.length() > 0 ;
+    }
+
+
+    /**
+     *
+     * @return an NFA
+     * */
+    private NFA term() {
+           NFA factor = new NFA(); //instantiate a new NFA that is empty
+//
+        while (more() && peek() != ')' && peek() != '|') {
+//            RegEx nextFactor = factor() ;
+//            factor = new Sequence(factor,nextFactor) ;
+       }
+//
+       return factor;
+
+    }
+
+
+    /**
+     *
+     *
+     * */
+    private NFA factor() {
+
+//        RegEx base = base() ;
+//
+//        while (more() && peek() == '*') {
+//            eat('*') ;
+//            base = new Repetition(base) ;
+//        }
+//
+       return null ;
+    }
+
+    /**
+     *
+     *
+     * */
+    private NFA base() {
+
+        switch (peek()) {
+            case '(':
+                eat('(') ;
+               NFA r = regEx() ;
+                eat(')') ;
+                return r ;
+//
+//            case '\\':
+//                eat ('\\') ;
+//                char esc = next() ;
+//                return new Primitive(esc) ;
+//
+                default:
+               return null;//new Primitive(next()) ;
+        }
+    }
+
+
+
+
+
 
 }
