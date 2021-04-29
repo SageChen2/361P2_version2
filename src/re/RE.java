cf9cec6c7f3f181c8b22753ffee690821f18fdd3
Sagechen1 <Chrisschenzixiao@gmail.com>
2021-04-29 08:07:11 -0600

    update all

--- src/re/RE.java
+++ src/re/RE.java
@@ -1,238 +1,291 @@
 package re;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-import fa.nfa.NFA;
 import fa.State;
+import fa.nfa.NFA;
 import fa.nfa.NFAState;
 
+/**
+ * Constructs an NFA for a given regular expression
+ *
+ * @author kellywilmot bridgettemilgie
+ */
 public class RE implements REInterface {
-    String regEx; // regular expression
-    int stateInc = 0;
+    String regEx = "";
+    int stateCounter = 0;
 
     /**
-     * constructor of the RE, this is the RegEx Parser
-     **/
-    public RE(String RegEx) {
-        this.regEx = RegEx;
-        stateInc = 0;
+     * Constructor
+     *
+     * @param regEx String of the regular expression
+     */
+    public RE(String regEx) {
+        this.regEx = regEx;
     }
 
     /**
-     * @return The NFA that is built by a given regular expression
+     * @return The NFA built from the regular expression
      */
-    @Override
     public NFA getNFA() {
         return regEx();
-
     }
 
-    /*
-     * Generate an NFA from a regular expression
+    /**
+     * Builds an NFA from the regular expression
      *
+     * @return NFA
      */
     private NFA regEx() {
+        NFA term = term();
 
-        NFA noUnion = term();
+        //If the regex requires a union operation
+        if (more() && peek() == '|') {
 
-        if(more() && peek() == '|'){
             eat('|');
-            NFA isRegex = regEx();
-            //System.out.println(isRegex.getABC());//print alphabet
-            return union(noUnion, isRegex);
-        }
-        else{
-            //System.out.println(noUnion.getABC());
-            return noUnion;
+            NFA regex = regEx();
+            return union(term, regex);
+            //If no union is needed, just return the simple NFA
+        } else {
+            return term;
         }
     }
 
     /**
-     * returns the next item of input without consuming it
+     * combine 2 NFAs into 1 NFA (the order does not matter)
      *
-     * @return
-     ***/
+     * @return the combined NFA
+     */
+    private NFA union(NFA nfa1, NFA nfa2) {
+        NFA result = new NFA();
+
+        ///start state(might be wrong)
+        //NFAState startState = new NFAState(String.valueOf(stateInc));
+
+        String startState = String.valueOf(stateCounter++);//might be wrong
+        result.addStartState(startState);
+        result.addNFAStates(nfa1.getStates());
+        result.addNFAStates(nfa2.getStates());
+        result.addTransition(startState, 'e', nfa1.getStartState().getName());
+        result.addTransition(startState, 'e', nfa2.getStartState().getName());
+        result.addAbc(nfa1.getABC());
+        result.addAbc(nfa2.getABC());
+
+        return result;
+
+    }
+
+
+    /**
+     * Looks at the regex to determine what it needs to build
+     *
+     * @return parsed portion of the regex in the form of an NFA
+     */
+    private NFA term() {
+        NFA factor = new NFA();
+
+        while (more() && peek() != ')' && peek() != '|') {
+            NFA fact = factor();
+
+            //If RE tries to process regex and there no operations to perform, just return simplest NFA
+            if (factor.getStates().isEmpty()) {
+                factor = fact;
+                //If there are multiple terms following each other, perform concatenate operation on the NFAs
+            } else {
+                factor = concat(factor, fact);
+
+            }
+        }
+
+        return factor;
+    }
+
+    /**
+     * Concatenates the two NFAs, when two NFAs are in the regex together
+     *
+     * @param reg1 - the NFA we are adding reg2 onto
+     * @param reg2 - additional NFA that follows reg1
+     * @return NFA that has been concatenated
+     */
+    private NFA concat(NFA reg1, NFA reg2) {
+        //Get final states and name of start state in second NFA
+        String reg2Start = reg2.getStartState().getName();
+        Set<State> reg1Finals = reg1.getFinalStates();
+
+        //Add all states from second NFA to first NFA
+        reg1.addNFAStates(reg2.getStates());
+
+        //Make sure both alphabets are included
+        reg1.addAbc(reg2.getABC());
+
+        Iterator<State> itr = reg1Finals.iterator();
+        while (itr.hasNext()) {
+            State state = itr.next(); // might be wrong
+            ((NFAState) state).setNonFinal(); //cast all the State objects into NFAState and set them to be non-final
+            reg1.addTransition(state.getName(), 'e', reg2Start);//from the 'non-final' states, use an empty string e, to transit to the start state of nfa2
+
+        }
+
+
+        return reg1;
+    }
+
+    /**
+     * A factor is a base followed by a possibly empty sequence of '*'.
+     *
+     * @return root NFA or root with the star operator if the regex has a '*'
+     */
+    private NFA factor() {
+        NFA root = root();
+
+        //If star op is needed, descend into recursion
+        while (more() && peek() == '*') {
+            eat('*');
+            root = star(root);
+        }
+        return root;
+    }
+
+    /**
+     * Handles the star operator
+     *
+     * @param root - NFA we are building upon
+     * @return NFA - the previous NFA has now incorporated the star operator
+     */
+    private NFA star(NFA root) {
+
+        NFA retNFA = new NFA();
+
+        //Make new start state
+        String start = "q" + stateCounter++;
+        //stateCounter++;
+        retNFA.addStartState(start);
+
+        //Make new final state
+        String finState = "q" + stateCounter++;
+        //stateCounter++;
+        retNFA.addFinalState(finState);
+
+        //Add all states from root to new NFA
+        retNFA.addNFAStates(root.getStates());
+
+        //Add empty transitions because star allows for 0 occurrences of term/NFA
+        retNFA.addTransition(start, 'e', finState);
+        retNFA.addTransition(finState, 'e', root.getStartState().getName());
+
+        //Tie new start to root NFA
+        retNFA.addTransition(start, 'e', root.getStartState().getName());
+
+        //Make sure old alphabet is included
+        retNFA.addAbc(root.getABC());
+
+
+        Iterator<State> itr = root.getFinalStates().iterator();
+        while (itr.hasNext()) {
+            State state = itr.next();
+            retNFA.addTransition(state.getName(), 'e', finState);
+
+            Iterator<State> itr2 = retNFA.getFinalStates().iterator();
+            while (itr2.hasNext()) {
+                State state2 = itr2.next();
+                if (state2.getName().equals(state.getName())) {
+                    ((NFAState) state2).setNonFinal();
+                }
+
+            }
+
+
+        }
+
+
+        return retNFA;
+    }
+
+    /**
+     * Root is a character, an escaped character, or a parenthesized regular expression.
+     *
+     * @return an NFA built from the next symbol or within the parenthesis
+     */
+    private NFA root() {
+        //Check if next symbol requires changing precedent using '()'
+        switch (peek()) {
+            case '(':
+                eat('(');
+                NFA reg = regEx();
+                eat(')');
+                return reg;
+            default:
+                return symbol(next());
+        }
+    }
+
+    /**
+     * Builds an NFA from the given character
+     *
+     * @param c Character to define transition on
+     * @return NFA from given character
+     */
+    private NFA symbol(char c) {
+        NFA nfa = new NFA();
+
+        //Make a new simple NFA with 2 states and a transition on char c
+        String s = "q" + stateCounter++;
+        nfa.addStartState(s);
+        String f = "q" + stateCounter++;
+        nfa.addFinalState(f);
+
+        nfa.addTransition(s, c, f);
+
+        Set<Character> alphabet = new LinkedHashSet<Character>();
+        alphabet.add(c);
+        nfa.addAbc(alphabet);
+        return nfa;
+
+    }
+
+    /**
+     * Peeks at the first index of the regex
+     *
+     * @return The next unprocessed character in the regex
+     */
     private char peek() {
         return regEx.charAt(0);
-
     }
 
     /**
-     * returns the next item of input and consumes it
+     * Processes the character and removes from the regex
+     *
+     * @param c Character to process
+     */
+    private void eat(char c) {
+        if (peek() == c) {
+            this.regEx = this.regEx.substring(1);
+        } else {
+            throw new RuntimeException("Received: " + peek() + "\n" + "Expected: " + c);
+        }
+    }
+
+    /**
+     * Moves the processed character, removing the character from the regex and returning it
+     *
+     * @return the character that was processed
      */
     private char next() {
         char c = peek();
         eat(c);
-
-        return 0;// placer holder
+        return c;
     }
 
     /**
-     * consumes the next item of input, failing if not equal to input item(char c).
-     */
-    private void eat(char c) {
-        if (peek() == c) { // peak() returns the next item of input without consuming it
-            this.regEx = this.regEx.substring(1);// the item at index 0 is consumed
-        } else {
-            throw new RuntimeException("Expected: " + c + "; got: " + peek());
-        }
-    }
-
-    /**
-     * checks if there is more input available.
+     * Evaluates if there are more characters to assess in the regex
      *
-     * @return true if the length of regular expression is greater than 0
-     **/
+     * @return boolean
+     */
     private boolean more() {
         return regEx.length() > 0;
     }
 
-
-    /**
-     *  combine 2 NFAs into 1 NFA (the order does not matter)
-     *
-     * @return the combined NFA
-     * */
-    private NFA union(NFA nfa1, NFA nfa2){
-            NFA result = new NFA();
-
-            ///start state(might be wrong)
-            //NFAState startState = new NFAState(String.valueOf(stateInc));
-
-            String startState = String.valueOf(stateInc++);//might be wrong
-            result.addStartState(startState);
-            result.addNFAStates(nfa1.getStates());
-            result.addNFAStates(nfa2.getStates());
-            result.addTransition(startState, 'e', nfa1.getStartState().getName());
-            result.addTransition(startState, 'e', nfa2.getStartState().getName());
-            result.addAbc(nfa1.getABC());
-            result.addAbc(nfa2.getABC());
-
-            return result;
-
-    }
-
-
-
-    /**
-     * @return an NFA
-     */
-    private NFA term() { //A term is a possibly empty sequence of factors.
-        NFA factor = new NFA(); // instantiate a new NFA that is empty
-
-        // while it has not reached the boundary of a term or the end of the input: (for example "term | term")
-        while (more() && peek() != ')' && peek() != '|') {
-            NFA nextFactor = factor();
-
-            //if NFA has no states
-            if (factor.getStates().isEmpty()) {
-                factor = nextFactor;
-            } else {//if there is more than one term in the NFA.
-
-                factor = concat(factor, nextFactor);
-
-            }
-
-
-        }
-        //
-        return factor;
-
-    }
-
-    /**
-     * to concatenate 2 NFAs (A regEx is a term and it is also an NFA), they are 2 terms seperated by '|'
-     * the order of concatenation is to add the nfa2 onto the nfa1(order matters)
-     * @return combined NFA of the first 2 NFAs
-     */
-    private NFA concat(NFA nfa1, NFA nfa2) {
-        //concatenation of NFA requires to 'link' the final states of the 1st NFA to the start state pf the 2nd NFA
-       // Set<State> finalStatesOfNfa1 = nfa1.getFinalStates(); // can be simplified
-        String startStateOfNfa2 = nfa1.getStartState().getName(); // there is only 1 start state
-
-        //need to combine alphabet, states and transitions
-
-        //combine all the states from nfa2 to nfa1 (the order might cause problem)
-        nfa1.addNFAStates(nfa2.getStates());
-
-        //combine the alphabets of nfa1 and nfa2 and store in the alphabet of nfa1
-        //nfa1.addAbc(nfa2.getABC());
-
-        //iterate through all the final states of nfa1 and set them to be non-final states
-        //also add nfa1's transitions to the start of nfa2
-        Iterator<State> itr = nfa1.getFinalStates().iterator();
-        while (itr.hasNext()) {
-            State state = itr.next(); // might be wrong
-            ((NFAState) state).setNonFinal(); //cast all the State objects into NFAState and set them to be non-final
-            nfa1.addTransition(state.getName(), 'e', startStateOfNfa2);//from the 'non-final' states, use an empty string e, to transit to the start state of nfa2
-
-        }
-
-        nfa1.addAbc(nfa2.getABC());//position might not matter
-
-        return nfa1;
-    }
-
-
-    /**
-     *
-     */
-    private NFA factor() {
-
-        NFA base = base();
-
-        //A factor is a base followed by a possibly empty sequence of '*'.
-        //while there is more input and the next item of input is a *
-        while (more() && peek() == '*') {
-            eat('*');
-
-            Set<State> setOfStates = base.getFinalStates(); //there exist multiple end states
-
-
-            //transition from an end state back to the start state
-            Iterator<State> itr = setOfStates.iterator();
-            while (itr.hasNext()) {
-                base.addTransition(itr.next().getName(), 'e', base.getStartState().getName());// might be wrong
-            }
-            NFAState starter = new NFAState(String.valueOf(stateInc++));    // create a new starter state
-            base.addState(starter.getName());   // add created state
-            base.addTransition(starter.getName(), 'e', base.getStartState().getName());    // new state needs transition
-            base.addStartState(starter.getName());  // set new starter to the start state
-            base.addFinalState(starter.getName());
-            // sequence is possibly empty, so new state also added to final states
-            //double check last few lines\
-        }
-
-        return base;
-    }
-
-    /**
-     * need to add alphabet in the base
-     */
-    private NFA base() {
-
-        //A base is a character, an escaped character, or a parenthesized regular expression.
-        NFA base = new NFA();
-        Set<Character> alphabet = new LinkedHashSet<Character>();
-
-        if (peek() == '(') {
-            eat('(');
-            base = regEx();// might be wrong
-            eat(')');
-
-        } else {
-            NFAState startS = new NFAState(String.valueOf(stateInc++));
-            NFAState endS = new NFAState(String.valueOf(stateInc++));
-            base.addStartState(startS.getName());
-            base.addFinalState(endS.getName());
-            base.addTransition(startS.getName(), next(), endS.getName());
-            //
-        }
-
-        return base;
-    }
 
 }
