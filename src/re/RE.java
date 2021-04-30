189a041915e32231cc3325ca0c1b740f9c9de168
DanielMcDougall <danielmcdougall@u.boisestate.edu>
2021-04-30 16:24:47 -0600

    refactoring

--- src/re/RE.java
+++ src/re/RE.java
@@ -82,60 +82,49 @@ public class RE implements REInterface {
      */
     private NFA term() {
         NFA startFactor = new NFA();
-
         while (more() && peek() != ')' && peek() != '|') {
             NFA newFactor = factor();
-
             //If a term is just an empty sequence of factors
             if (startFactor.getStates().isEmpty()) {
                 startFactor = newFactor;
             } else {//concatentae the term if there are multple factor
-                startFactor = concat(startFactor, newFactor);
-
+                startFactor = combine(startFactor, newFactor);
             }
         }
-
         return startFactor;
     }
 
     /**
-     *
-     * Combing 2 NFAs in a specific order, nfa2 onto nfa1
+     * combine two nfas
      * @param nfa1 - base nfa
-     * @param nfa2 - nfa to be combined to the base nfa
-     * @return concatenated NFA
+     * @param nfa2 - nfa added to nfa1
+     * @return combined nfa
      */
-    private NFA concat(NFA nfa1, NFA nfa2) {
-        //Get final states of nfa1
+    private NFA combine(NFA nfa1, NFA nfa2) {
+        // must use a set for multiple final states
         Set<State> nfa1FinalStates = nfa1.getFinalStates();
-
-        //add all the states from nfa2 to nfa1
+        //add states from nfa2 to base
         nfa1.addNFAStates(nfa2.getStates());
-
-        //combing the alphabet
         nfa1.addAbc(nfa2.getABC());
 
         Iterator<State> itr = nfa1FinalStates.iterator();
         while (itr.hasNext()) {
             State state = itr.next();
-            ((NFAState) state).setNonFinal(); //cast all the State objects into NFAState and set them to be non-final
-            nfa1.addTransition(state.getName(), 'e', nfa2.getStartState().getName());//from the 'non-final' states, use an empty string e, to transit to the start state of nfa2
-
+             //states are now non-final nfa states
+            ((NFAState) state).setNonFinal();
+            //empty transition used to rach start of nfa2
+            nfa1.addTransition(state.getName(), 'e', nfa2.getStartState().getName());
         }
-
-
         return nfa1;
     }
 
     /**
-     * build a factor from base, the base might be repetitive because of the *
-     *
-     * @return an NFA built from a base(regex), either with or without the '*'
+     * Builds factor from a base, but checks for repitition if needed
+     * @return an nfa built from a base
      */
     private NFA factor() {
         NFA base = base();
-
-      
+        //if the regex is longer and the next char is star operator
         while (more() && peek() == '*') {
             eat('*');
             base = star(base);
@@ -144,129 +133,93 @@ public class RE implements REInterface {
     }
 
     /**
-     * Handles the star operator
-     *
-     * @param base - NFA we are building upon
-     * @return NFA - the previous NFA has now incorporated the star operator
+     * Creates a case accepting the star operator if needed
+     * @param base NFA to build on
+     * @return NFA that now includes a star operator
      */
     private NFA star(NFA base) {
-
-        NFA retNFA = new NFA();
-
+        // new simple nfa to be returned
+        NFA starBase = new NFA();
         NFAState startS = new NFAState(String.valueOf(stateInc++));
         NFAState endS = new NFAState(String.valueOf(stateInc++));
-
-        //add new start state
-        retNFA.addStartState(startS.getName());
-
-        //add new final state
-        retNFA.addFinalState(endS.getName());
-
-        //Add all states from root to new NFA
-        retNFA.addNFAStates(base.getStates());
-
-        //Add empty transitions because star allows for 0 occurrences of term/NFA
-        retNFA.addTransition(startS.getName(), 'e', endS.getName());
-        retNFA.addTransition(endS.getName(), 'e', base.getStartState().getName());
-
-        //Tie new start to root NFA
-        retNFA.addTransition(startS.getName(), 'e', base.getStartState().getName());
-
-        //Make sure old alphabet is included
-        retNFA.addAbc(base.getABC());
-
+        starBase.addStartState(startS.getName());
+        starBase.addFinalState(endS.getName());
+        //Add all states from root to starBase
+        starBase.addNFAStates(base.getStates());
+        //transitions need to be empty because of star operator
+        starBase.addTransition(startS.getName(), 'e', endS.getName());
+        starBase.addTransition(endS.getName(), 'e', base.getStartState().getName());
+        //new starting state should connect to base
+        starBase.addTransition(startS.getName(), 'e', base.getStartState().getName());
+        starBase.addAbc(base.getABC());
 
         Iterator<State> itr = base.getFinalStates().iterator();
         while (itr.hasNext()) {
             State state = itr.next();
-            retNFA.addTransition(state.getName(), 'e', endS.getName());
-
-            Iterator<State> itr2 = retNFA.getFinalStates().iterator();
+            starBase.addTransition(state.getName(), 'e', endS.getName());
+            Iterator<State> itr2 = starBase.getFinalStates().iterator();
             while (itr2.hasNext()) {
                 State state2 = itr2.next();
                 if (state2.getName().equals(state.getName())) {
                     ((NFAState) state2).setNonFinal();
                 }
-
             }
-
-
         }
-
-
-        return retNFA;
+        return starBase;
     }
 
     /**
      * Root is a character, an escaped character, or a parenthesized regular expression.
-     *
      * @return an NFA built from the next symbol or within the parenthesis
      */
     private NFA base() {
-        //Check if next symbol requires changing precedent using '()'
+        //check parenthesis case for base
         switch (peek()) {
             case '(':
                 eat('(');
                 NFA reg = regEx();
                 eat(')');
                 return reg;
+            //if not build a base, simple 2 state nfa
             default:
-                return symbol(next());
+            NFA defNfa = new NFA();
+            //fill out the 5 tuple
+            NFAState startS = new NFAState(String.valueOf(stateInc++));
+            NFAState endS = new NFAState(String.valueOf(stateInc++));  
+            char nextChar = next();
+            defNfa.addStartState(startS.getName());
+            defNfa.addFinalState(endS.getName());
+            defNfa.addTransition(startS.getName(), nextChar, endS.getName());
+            Set<Character> alphabet = new LinkedHashSet<Character>();
+            alphabet.add(nextChar);
+            defNfa.addAbc(alphabet);
+            return defNfa;
         }
     }
 
     /**
-     * Builds an NFA from the given character
-     *
-     * @param symbol Character to define transition on
-     * @return NFA from given character
-     */
-    private NFA symbol(char symbol) {
-        NFA nfa = new NFA();
-
-        //Make a new simple NFA with 2 states and a transition on char c
-        NFAState startS = new NFAState(String.valueOf(stateInc++));
-
-        NFAState endS = new NFAState(String.valueOf(stateInc++));
-
-        nfa.addStartState(startS.getName());
-        nfa.addFinalState(endS.getName());
-
-        nfa.addTransition(startS.getName(), symbol, endS.getName());
-
-        Set<Character> alphabet = new LinkedHashSet<Character>();
-        alphabet.add(symbol);
-        nfa.addAbc(alphabet);
-        return nfa;
-
-    }
-
-    /**
-     * Peeks at the first index of the regex
-     *
-     * @return The next unprocessed character in the regex
+     * just peek at the next char, no action or removal
+     * @return The next char in the regex
      */
     private char peek() {
         return regEx.charAt(0);
     }
 
     /**
-     * Processes the character and removes from the regex
-     *
-     * @param c Character to process
+     * process char and then return it (removing it as well)
+     * @param c Char to process
      */
     private void eat(char c) {
         if (peek() == c) {
             this.regEx = this.regEx.substring(1);
         } else {
-            throw new RuntimeException("Received: " + peek() + "\n" + "Expected: " + c);
+            System.err.println("Peek() expected a char but ran into an error");
         }
     }
 
     /**
-     * Moves the processed character, removing the character from the regex and returning it
-     *
-     * @return the character that was processed
+     * remove the next char and return it
+     * @return the next char
      */
     private char next() {
         char c = peek();
@@ -275,9 +228,8 @@ public class RE implements REInterface {
     }
 
     /**
-     * Evaluates if there are more characters to assess in the regex
-     *
-     * @return boolean
+     * Helper method to determine length of regex
+     * @return boolean if regex has more characters
      */
     private boolean more() {
         return regEx.length() > 0;
